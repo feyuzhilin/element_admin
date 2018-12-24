e6cb1893b4cef36d4bd0668a398aabe3df6c0683
CapFour <1009342713@qq.com>
2018-12-24 19:21:15 +0800

    element-ui引入完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,37 +1,13 @@
 <template>
   <div class="login">
     <div class="login-box">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="用户名">
-          <el-input v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item label="密码">
-          <el-input v-model="form.password"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit">登录</el-button>
-          <el-button>重置</el-button>
-        </el-form-item>
-      </el-form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      form: {
-        name: "",
-        password: ""
-      }
-    }
-  },
-  methods: {
-    onSubmit() {
-      console.log("submit!")
-    }
-  }
+
 }
 </script>
 
